6c406b47cc6647cbf01eec69c79d67d0299726d6
Edward Hatrick-Smith <edward.hatrick-smith@worldnomadsgroup.com>
2017-05-28 10:14:09 +1000

    Looks nice now

--- index.php
+++ index.php
@@ -26,18 +26,18 @@
         <h2>Floors no longer need to be cold</h2>
         <p>Think of those times when you get home on a cold wet night, have to turn on the electric heater and wait for it to heat up. Even then it is only heating one small area of your home.</p>
         <p>With Radiant Electric Heating you will come home to a warm floor and home when using an automatic programmable thermostat. Keep your family warm and healthy with Radiant Electric Heating.</p>
-        <a class="btn btn--secondary" href="heating-options/floor-heating-options.php">Learn more here</a>
+        <a class="btn btn--secondary" href="heating-options/floor-heating-options.php">Learn more</a>
       </div>
       <div class="col-xs-12 col-sm-6 col-md-4 section">
         <h2>Save money on household heating</h2>
         <p>Under floor heating is an efficient way of heating your whole home from the floor up. This will help reduce your overall heating bill and save you money.</p>
         <p>We're experts in this field and are happy to help you with you floor heating project. We're here to help you learn how to do it yourself. We can even supply the equipment for D.I.Y. installations.</p>
-        <a class="btn btn--secondary" href="diy/do-it-yourself.php">Learn more here</a>
+        <a class="btn btn--secondary" href="diy/do-it-yourself.php">Learn more</a>
       </div>
       <div class="col-xs-12 col-sm-6 col-md-4 section">
         <h2>Contact us today for a free site measure and quotation or send us your plans</h2>
         <p>If you are starting a renovation project, building a new home or simply want to know more about under floor heating, contact us and we will help you out.</p>
-        <a class="btn btn--secondary" href="contacts.php">Contact us here</a>
+        <a class="btn btn--secondary" href="contacts.php">Contact us</a>
       </div>
     </section>
   </div>
